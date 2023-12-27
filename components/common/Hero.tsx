01f7548e0b64314ee465c6ec074e701fcdd98f8b
edidiesky <essienedidiong1000@gmail.com>
2023-12-27 15:32:42 -0800

    new fixes

--- components/common/Hero.tsx
+++ components/common/Hero.tsx
@@ -1,5 +1,7 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
+import SplitType from "split-type";
+import gsap from "gsap";
 import Button from "../ui/button";
 import Image from "next/image";
 
@@ -23,6 +25,24 @@ const Hero: React.FC<HeroProps> = ({
   buttonpath2,
   discord
 }) => {
+  // useEffect(() => {
+  //   const text = new SplitType('span')
+  //   gsap.timeline().fromTo(
+  //     text.chars,
+  //     { y: 870 }, // Initial skewY value
+  //     {
+  //       skewY: 0,
+  //       y: 0,
+  //       duration: 2,
+  //       delay: 4,
+  //       ease: "power4.Out",
+  //       stagger: {
+  //         amount: 0.8,
+  //       },
+  //     }
+  //   );
+  // }, []);
+
   return (
     <div className="w-full z-10 overflow-hidden relative font-sans bg-custom_1 py-4">
       <Image
@@ -56,52 +76,37 @@ const Hero: React.FC<HeroProps> = ({
       />
       <div className="w-90 py-36 pb-40 mx-auto w-custom gap-12 flex flex-col item-center justify-center items-center">
         <h2 className="text-5xl w-full md:w-full lg:w-custom_2 lg:text-6xl sm:text-start md:text-center font-bold text-white gap-8">
-          <div className="w-full overflow-hidden">
+         <div className="overflow-hidden w-full">
             <span
-              data-aos="fade-up"
-              data-aos-duration="700"
-              data-aos-delay="200"
               className="text-base md:text-base mx-0 sm:mx-auto uppercase text-start sm:text-start md:text-center w-full md:w-4/5 font-light text-text_grey_1 block">{toptext}</span>
 
-          </div>
-
-          <div className="w-full overflow-hidden">
+         </div>
+         <div className="overflow-hidden w-full">
             <span
-              data-aos="fade-up"
-              data-aos-duration="700"
-              data-aos-delay="400"
               style={{ lineHeight: "1.3" }} className="py-4 block">
               {maintext}
             </span>
-          </div>
+         </div>
+         
+
           <div className="w-full overflow-hidden">
             <span
-              data-aos="fade-up"
-              data-aos-duration="700"
-              data-aos-delay="600"
               className="text-base md:text-xl w-full md:w-4/5 leading-loose mx-0 sm:text-start md:text-center sm:mx-0 md:mx-auto font-light text-text_grey_1 block">{bottomtext}</span>
+
           </div>
         </h2>
         <div className="flex-col sm:flex-row sm:justify-start md:justify-center mx-0 sm:mx-0 sm:w-custom-2 w-full md:mx-auto flex gap-4 sm:gap-4 md:gap-8">
           <Button
-            data-aos="zoom-in"
-            data-aos-duration="600"
-            data-aos-delay={"300"}
             buttontext1='Learn More'
             type={true}
           />
 
           {discord ? <Button
-            data-aos="zoom-in"
-            data-aos-duration="600"
-            data-aos-delay={"600"}
+
             buttontext1={buttontext2 ? buttontext2 : 'Launch App'}
             discord={discord}
           /> :
             <Button
-              data-aos="zoom-in"
-              data-aos-duration="600"
-              data-aos-delay={"600"}
               buttontext1='Launch App'
             />
           }
